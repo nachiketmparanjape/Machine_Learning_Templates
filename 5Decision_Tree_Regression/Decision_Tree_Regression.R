6d3c23bdfc81b745519727cf110c5927e9da3ecb
nachiketmparanjape <nachiketm.paranjape@gmail.com>
2017-01-05 18:31:58 -0800

    R template for decision tree regression

--- 5Decision_Tree_Regression/Decision_Tree_Regression.R
+++ 5Decision_Tree_Regression/Decision_Tree_Regression.R
@@ -1,4 +1,4 @@
-# Regression Template
+# Decision Tree Regression
 
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
@@ -16,28 +16,31 @@ dataset = dataset[2:3]
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting the Regression Model to the dataset
-# Create your regressor here
+# Fitting the Decision Tree Regression to the dataset
+#install.packages("rpart")
+library(rpart)
+regressor <- rpart(formula = Salary ~ ., data = dataset,
+                   control = rpart.control(minsplit = 1))
 
 # Predicting a new result
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
-# Visualising the Regression Model results
+# Visualising the Decision Tree model results
 # install.packages('ggplot2')
-library(ggplot2)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
-  xlab('Level') +
-  ylab('Salary')
+#library(ggplot2)
+#ggplot() +
+#  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+#             colour = 'red') +
+#  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
+#            colour = 'blue') +
+#  ggtitle('Truth or Bluff (Regression Model)') +
+#  xlab('Level') +
+#  ylab('Salary')
 
-# Visualising the Regression Model results (for higher resolution and smoother curve)
+# Visualising the Decision Tree Regression Model results (for higher resolution and smoother curve)
 # install.packages('ggplot2')
 library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
              colour = 'red') +

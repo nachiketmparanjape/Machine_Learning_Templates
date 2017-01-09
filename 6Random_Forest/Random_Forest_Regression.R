9f3de0cb4889c0b1fe510f81ef36b62d3bfd1f89
nachiketmparanjape <nachiketm.paranjape@gmail.com>
2017-01-08 16:06:30 -0800

    R - Created and tested Random Forest Template

--- 6Random_Forest/Random_Forest_Regression.R
+++ 6Random_Forest/Random_Forest_Regression.R
@@ -17,32 +17,27 @@ dataset = dataset[2:3]
 # test_set = scale(test_set)
 
 # Fitting the Regression Model to the dataset
-# Create your regressor here
+#install.packages('randomForest')
+library(randomForest)
+set.seed(1234)
+regressor <- randomForest(x = dataset[1], #Array
+                          y = dataset$Salary, #Vector
+                          ntree = 500)
 
 # Predicting a new result
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
 # Visualising the Regression Model results
-# install.packages('ggplot2')
-library(ggplot2)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
-  xlab('Level') +
-  ylab('Salary')
 
 # Visualising the Regression Model results (for higher resolution and smoother curve)
 # install.packages('ggplot2')
 library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.001)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
              colour = 'red') +
   geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
             colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
+  ggtitle('Truth or Bluff (Random Forest)') +
   xlab('Level') +
   ylab('Salary')

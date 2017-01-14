cb5900aa3417df4f7b0f332bac9a738b0d05ecbf
nachiketmparanjape <nachiketm.paranjape@gmail.com>
2017-01-14 14:06:08 -0800

    Created visualization for the clusters

--- 15Hierarchical_Clustering/Hierarchical_Clustering.py
+++ 15Hierarchical_Clustering/Hierarchical_Clustering.py
@@ -7,4 +7,34 @@ import matplotlib.pyplot as plt
 
 #Import data
 dataset = pd.read_csv("Mall_Customers.csv")
-X = dataset.iloc[]
+X = dataset.iloc[:,[3,4]].values
+
+#Hierarchical Clustering
+#Using the dendrogram
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+plt.title("Dendrogram")
+plt.xlabel("Customers")
+plt.ylabel("Euclidean Distances")
+
+#Optimal Number of Clusters = 5
+#Fitting
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
+y_hc = hc.fit_predict(X)
+
+#Visualization
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 20, c = 'red', label = 'Careful')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 20, c = 'blue', label = 'Standard')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 20, c = 'green', label = 'Target')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 20, c = 'cyan', label = 'Careless')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 20, c = 'magenta', label = 'Sensible')
+
+#Centroids
+#plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], s = 50, c = 'yellow', label = 'Centroids')
+
+plt.title('Clusters of Clients')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending score (1-100)')
+plt.legend()
+plt.show()

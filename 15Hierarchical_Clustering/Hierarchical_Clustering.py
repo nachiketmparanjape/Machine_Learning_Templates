d8a7474ea8c09baf219a1524c054c87e93c8281d
nachiketmparanjape <nachiketm.paranjape@gmail.com>
2017-01-14 14:00:24 -0800

    Created hierarchical clustering template in python

---
+++ 15Hierarchical_Clustering/Hierarchical_Clustering.py
@@ -0,0 +1,10 @@
+#Hierarchical Clustering Template
+
+#Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Import data
+dataset = pd.read_csv("Mall_Customers.csv")
+X = dataset.iloc[]

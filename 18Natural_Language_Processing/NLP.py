5c43363be259da2f9a0b4330f8e8a208dd6ae915
nachiketmparanjape <nachiketm.paranjape@gmail.com>
2017-01-18 23:35:54 -0800

    Created the sparse matrix using the bag of words model. Performed classification using 3 algoriths - 1. Naive Bayes 2. Random Forest 3. Logistic Regression

--- 18Natural_Language_Processing/NLP.py
+++ 18Natural_Language_Processing/NLP.py
@@ -4,6 +4,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+def F1_Score(cm):
+    
+    """ Used to calculate performace of the classification method"""
+
+    accuracy = (float(cm[0,1]) + cm[1,0])/(float(cm[0,0]) + cm[1,1]+ cm[0,1] + cm[1,0])
+    precision = round(float(cm[0,0])/(cm[0,0]+cm[0,1]),4)
+    recall = round(float(cm[0,0])/(cm[0,0]+cm[1,0]),4)
+    F1 = round(2 * precision * recall / (precision + recall),2)
+    
+    print ("------------------------------------------------------")
+    print (method + "\n")
+    print ("Accuracy - " + str(accuracy))
+    print ("Precision - " + str(precision))
+    print ("Recall - " + str(recall))
+    print ("------------------------------------------------------")
+    print ("F1 Score - " + str(F1))
+    print ("------------------------------------------------------\n")
+
+
 #Importing data
 dataset = pd.read_csv("Restaurant_Reviews.tsv",delimiter = "\t", quoting = 3)
 #quoting = 3 for ignoring double quotes in the dataset
@@ -43,3 +62,73 @@ for i in range(len(dataset)):
     corpus.append(review)
     
 #Creating Bag of words model
+from sklearn.feature_extraction.text import CountVectorizer
+
+cv = CountVectorizer(max_features=1500)
+
+#Creating a sparse matrix to analyze the data
+X = cv.fit_transform(corpus).toarray()
+
+y = dataset.iloc[:,1]
+
+
+#Now the prediction = which classification to use?
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+#---------------------------------------------------------------------------
+
+#1. Naive Bayes
+global method
+method = "Naive Bayes"
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+F1_Score(cm)
+
+#-----------------------------------------------------------------------
+
+#2. Random Forest
+method = "Random Forest"
+
+# Fitting Random Forest Classification to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
+F1_Score(cm)
+
+#--------------------------------------------------------------------------
+
+#3. Logistic Regression
+method = "Logistic Regression"
+
+#Fitting - logistic regression
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train,y_train)
+
+#Predicting the test set results
+y_pred = classifier.predict(X_test)
+
+#Evaluating the classifier performance
+#Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
+F1_Score(cm)
